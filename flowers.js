95dc04512a7ba161af211afef8e3fbd52295a6dd
Amira Hashem <121437507+Amirahashem@users.noreply.github.com>
2025-03-07 19:53:19 +0200

    Add files via upload

---
+++ flowers.js
@@ -0,0 +1,120 @@
+// Create Popup With The Image
+let ourGallery = document.querySelectorAll(".gallery img");
+
+ourGallery.forEach(img => {
+
+  img.addEventListener('click', (e) => {
+
+    // Create Overlay Element
+    let overlay = document.createElement("div");
+
+    // Add Class To Overlay
+    overlay.className = 'popup-overlay';
+
+    // Append Overlay To The Body
+    document.body.appendChild(overlay);
+
+    // Create The Popup Box
+    let popupBox = document.createElement("div");
+
+    // Add Class To The Popup Box
+    popupBox.className = 'popup-box';
+
+    if (img.alt !== null) {
+
+      // Create Heading
+      let imgHeading = document.createElement("h3");
+
+      // Create text For Heading
+      let imgText = document.createTextNode(img.alt);
+
+      // Append The Text To The Heading
+      imgHeading.appendChild(imgText);
+
+      // Append The Heading To The Popup Box
+      popupBox.appendChild(imgHeading);
+
+    }
+
+    // Create The Image
+    let popupImage = document.createElement("img");
+
+    // Set Image Source
+    popupImage.src = img.src;
+
+    // Add Image To Popup Box
+    popupBox.appendChild(popupImage);
+
+    // Append The Popup Box To Body
+    document.body.appendChild(popupBox);
+
+    // Create The Close Span
+    let closeButton = document.createElement("span");
+
+    // Create The Close Button Text
+    let closeButtonText = document.createTextNode("X");
+
+    // Append Text To Close Button
+    closeButton.appendChild(closeButtonText);
+
+    // Add Class To Close Button
+    closeButton.className = 'close-button';
+
+    // Add Close Button To The Popup Box
+    popupBox.appendChild(closeButton);
+
+  });
+
+});
+
+// Close Popup
+document.addEventListener("click", function (e) {
+
+  if (e.target.className == 'close-button') {
+
+    // Remove The Current Popup
+    e.target.parentNode.remove();
+
+    // Remove Overlay
+    document.querySelector(".popup-overlay").remove();
+
+  }
+
+});
+
+// Select All Bullets
+const allBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+// Select All Links
+const allLinks = document.querySelectorAll(".links a");
+
+function scrollToSomewhere(elements) {
+
+  elements.forEach(ele => {
+
+    ele.addEventListener("click", (e) => {
+
+      e.preventDefault();
+
+      document.querySelector(e.target.dataset.section).scrollIntoView({
+
+        behavior: 'smooth'
+
+      });
+
+    });
+
+  });
+
+}
+
+scrollToSomewhere(allBullets);
+scrollToSomewhere(allLinks);
+
+
+// $(document).ready(function(){
+//   $('.more').click(function(){
+//      $('.cont_about').show()
+//     })
+// });
+
